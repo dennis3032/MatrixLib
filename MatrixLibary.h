765d6c49cac7a8787f07c320f966d3bfa43a5085
dennis3032 <117104336+dennis3032@users.noreply.github.com>
2022-10-31 19:40:36 +0300

    Update MatrixLibary.h

--- MatrixLibary.h
+++ MatrixLibary.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #ifdef MATRIXLIBARY_EXPORTS
-#define MATRIXLIBARY_API __declspec(dllexport) //модификатор обьявления ф-ий
+#define MATRIXLIBARY_API __declspec(dllexport) //РјРѕРґРёС„РёРєР°С‚РѕСЂ РѕР±СЊСЏРІР»РµРЅРёСЏ С„-РёР№
 #else
 #define MATRIXLIBARY_API __declspec(dllimport)
 #endif
 
-//extern "C" указывает, что функция определена в другом месте и использует соглашение о вызовах на языке C.
 
 extern "C" MATRIXLIBARY_API struct matrix
 {
@@ -15,17 +14,12 @@ extern "C" MATRIXLIBARY_API struct matrix
 	int razmer;
 };
 
-//extern "C" MATRIXLIBARY_API int** initMatrix(matrix * m, int size);
-
-//extern "C" MATRIXLIBARY_API void deletMatrix(int** ptr, int size);
 
 extern "C" MATRIXLIBARY_API int** multiplication(int** ptr_A, int** ptr_B, int size);
 
 extern "C" MATRIXLIBARY_API int** addition(int** ptr_A, int** ptr_B, int size);
 
-extern "C" MATRIXLIBARY_API int** subtraction(int** ptr_A, int** ptr_B, int size);
-
-//extern "C" MATRIXLIBARY_API int** stepen(int** ptr_A, int size, int n);
+extern "C" MATRIXLIBARY_API int** subtraction(int** ptr_A, int** ptr_B, int size)
 
 extern "C" MATRIXLIBARY_API int** reverse(int** ptr_A, int size);
 
